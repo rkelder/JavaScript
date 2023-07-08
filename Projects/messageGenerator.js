d4c306fb637e1664af173f231f1b4bc7883ad4c0
rkelder <roderik.kelder@gmail.com>
2023-07-08 17:35:17 +0200

    messageGenerator works - not yet completely random

--- Projects/messageGenerator.js
+++ Projects/messageGenerator.js
@@ -1,6 +1,25 @@
-// Pathname for node: "C:\Users\roder\OneDrive\Documents\Projects\JavaScript\Projects\messageGenerator.js"
+function messageGenerator() {
+    const messages = {
+        message_1: "Coding like poetry should be short and concise. ― Santosh Kalwar",
+        message_2: "It’s not a bug; it’s an undocumented feature. ― Anonymous",
+        message_3: "First, solve the problem. Then, write the code. – John Johnson",
+        message_4: "Code is like humor. When you have to explain it, it’s bad. – Cory House",
+        message_5: "Make it work, make it right, make it fast. – Kent Beck"
+    }
 
-/* Message generator functions:
+    const randomNumber = Math.floor(Math.random() * 6);
+    const messageKey = 'message_' + (randomNumber + 1);
+
+    console.log(messages[messageKey]);
+}
+
+messageGenerator()
+
+/* 
+
+Pathname for node: "C:\Users\roder\OneDrive\Documents\Projects\JavaScript\Projects\messageGenerator.js"
+
+ Message generator functions:
 
 * Return a pre-defined message using console.log
 * The pre-defined message is one out of five options
