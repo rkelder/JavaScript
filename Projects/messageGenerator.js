03dd1aca542ffaf8dd78e95ac27ac5fdf0f9d1c8
rkelder <roderik.kelder@gmail.com>
2023-07-08 17:15:26 +0200

    messageGenerator  - added required functionality

---
+++ Projects/messageGenerator.js
@@ -0,0 +1,11 @@
+// Pathname for node: "C:\Users\roder\OneDrive\Documents\Projects\JavaScript\Projects\messageGenerator.js"
+
+/* Message generator functions:
+
+* Return a pre-defined message using console.log
+* The pre-defined message is one out of five options
+* The pre-defined messages are stored in one object
+* The messages are chosen using math.random
+* If the randomly selected message is the same as the one previously chosen, a different one will now be chosen.
+
+*/
